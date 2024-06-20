1057feb2c4f180389c91c64f090d4dfe04e54b88
Carson Wolber <cwolber@meta.com>
2024-06-20 00:46:53 -0700

    added the submit handler for the create form

--- frontend/src/CreateForm.jsx
+++ frontend/src/CreateForm.jsx
@@ -21,20 +21,48 @@ function CreateForm( {view , closeView} ) {
 
     //when any form input updates, name/value should update to reflect this
     const handleChange = (e) => {
-        const { name, value } = e.target || {}; // Destructure target from event if it exists
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
+        const { name, type } = e.target;
+        if (type === 'file') {  // if the object changed was the image, we need to access the target file array
+            const file = e.target.files[0];
+            setFormData(prevState => ({
+                ...prevState,
+                [name]: file
+            }));
+        } else {
+            const { value } = e.target;
+            setFormData(prevState => ({
+                ...prevState,
+                [name]: value
+            }));
+        }
     };
 
     //because react-select does not process event objects in the same way as vanilla tags it needs it's own handler
     const handleCategoryChange = (selectedOption) => {
         setFormData(prevState => ({
             ...prevState,
-            category: selectedOption // Set the whole option as the value
+            category: selectedOption // Set the whole option object as the value
         }));
     }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // prevent refreash
+        const formDataToSend = new FormData();
+        formDataToSend.append('title', formData.title);
+        formDataToSend.append('author', formData.author);
+        formDataToSend.append('category', formData.category.value); // category is an object because of react-select so we need to specify value
+        formDataToSend.append('coverart', formData.image);
+        try {
+            const response = await fetch('https://needtosetrealendpoint', { 
+                method: 'POST',
+                body: formDataToSend,
+            });
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
     const categories = [
         {value: 'recent', label: 'Recent'},
@@ -48,7 +76,7 @@ function CreateForm( {view , closeView} ) {
             <div className='form-content'>
             <span className="close" onClick={closeView}>&times;</span>
             <h3>Create New Kudos Board</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Cover Art:</label>
                     <input type='file' name="coverart" accept='image/jpg, image/jpeg, image/gif, image/png' onChange={handleChange}/>
                 <label>Title:</label>
@@ -65,7 +93,7 @@ function CreateForm( {view , closeView} ) {
                 />
                 <label>Author:</label>
                 <input type="text" name="author" value={formData.author} onChange={handleChange} required/>
-                <button type="submit">Submit</button>
+                <button type="submit">submit</button>
             </form>
             </div>
         </div>
